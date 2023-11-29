27033a1ec0f4a2a2dde36b6446672bb096c4e557
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2023-11-29 18:33:16 +0100

    Update report_kpis.sql

--- report_kpis.sql
+++ report_kpis.sql
@@ -1,23 +1,26 @@
-use schedulazione;
-CREATE TABLE KPI_VALORE (
+-- let's create a monthly report for KPIs created by us using a database monthly provided
+-- we create the table where we store the KPIs
+use calls;
+CREATE TABLE KPI_value (
   Centro VARCHAR(255),
   KPI_1 DECIMAL(10, 2),
   KPI_2 DECIMAL(10, 2),
-  KPI_3 VARCHAR(255),
+  KPI_3 datetime,
   KPI_4 DECIMAL(10, 2),
   KPI_5 DECIMAL(10, 2),
   Mese datetime
 );
-INSERT INTO KPI_VALORE(
+-- calculating KPIs and inserting into the tbale KPI_VALORE that will be our final report
+INSERT INTO KPI_value(
 SELECT
   Centro,
-  `% reale Utilizzo Schedulatore` / 100 as KPI_1,
+  `% time_on_call` / 100 as KPI_1,
   (CASE
-    WHEN (`Numero Medio Operatori per Run di Schedulazione` / `Numero Medio Giornaliero Operatori Disponibili`) > 1 THEN ROUND(1, 2)
-    ELSE ROUND((`Numero Medio Operatori per Run di Schedulazione` / `Numero Medio Giornaliero Operatori Disponibili`), 2)
+    WHEN (`scheduled calls` / `effective calls`) > 1 THEN ROUND(1, 2)
+    ELSE ROUND((`scheduled calls` / `effective calls`), 2)
   END) as KPI_2,
-  `Orizzonte Medio` as KPI_3,
-  `% Media Odl Validi e Schedulati` / 100 as KPI_4,
-  ROUND((`Numero Run` / 22), 2) as KPI_5, cast(periodo as datetime) as Mese
-FROM schedulazione.`db_completo (1)`);
-select * from schedulazione.KPI_VALORE;
+  `time_calls` as KPI_3,
+  `% avarage_time_call` / 100 as KPI_4,
+  ROUND((`number_of_call_per_day` / 22), 2) as KPI_5, period as Mese
+FROM calls.`db_completo (1)`);
+select * from calls.KPI_value;
